1bb4f1309c8e3c8501f343db03ea371dca551db4
Tính Trần <tranleductinh@gmail.com>
2025-09-19 00:14:28 +0700

    feat: enhance user dialog functionality and styling

--- script.js
+++ script.js
@@ -8,8 +8,26 @@ const dialogForm = document.getElementById("dialogForm");
 const dialogFormDelete = document.getElementById("dialogFormDelete");
 const userManagement = document.querySelector(".user-management__empty");
 const listUser = document.getElementById("list_user");
+const dialogSubmit = document.getElementById("dialogSubmit");
 
-function openDialog() {
+const userName = document.getElementById("user-name");
+const userEmail = document.getElementById("user-email");
+const userPhone = document.getElementById("user-phone");
+let dialogId = null;
+
+function openDialog(id) {
+  const dialogTitle = document.getElementById("dialogTitle");
+  if (id) {
+    const user = users.find((u) => u.id === id);
+    dialogTitle.innerText = "Edit User";
+    dialogSubmit.innerText = "Save Changes";
+    userName.value = user.userName;
+    userEmail.value = user.userEmail;
+    userPhone.value = user.userPhone;
+    dialogId = id;
+  } else {
+    dialogTitle.innerText = "Add User";
+  }
   document.getElementById("error-name").innerText = "";
   document.getElementById("error-phone").innerText = "";
   dialog.show();
@@ -38,10 +56,10 @@ function renderUser() {
               <p class="user-card__email">Email: ${u.userEmail}</p>
               <p class="user-card__phone">Phone: ${u.userPhone}</p>
               <div class="user-card__actions">
-                <button class="user-card__button user-card__button--edit">
+                <button class="user-card__button user-card__button--edit" onclick=openDialog(${u.id})>
                   Edit
                 </button>
-                <button class="user-card__button user-card__button--delete" onClick=deleteUser(${u.id})>
+                <button class="user-card__button user-card__button--delete" onclick=deleteUser(${u.id})>
                   Delete
                 </button>
               </div>
@@ -54,7 +72,7 @@ function renderUser() {
 renderUser();
 
 function deleteUser(id) {
-  deleteDialog()
+  deleteDialog();
   dialogFormDelete.addEventListener("submit", function (e) {
     e.preventDefault();
     users = users.filter((u) => u.id !== id);
@@ -69,25 +87,54 @@ function saveUser() {
 
 dialogForm.addEventListener("submit", function (e) {
   e.preventDefault();
-  const userName = document.getElementById("user-name").value;
-  const userEmail = document.getElementById("user-email").value;
-  const userPhone = document.getElementById("user-phone").value;
-  if (userName.length < 2) {
-    document.getElementById("error-name").innerText = "Tên phải ≥ 2 ký tự";
-    return;
+  if (dialogId) {
+    const user = users.findIndex((u) => u.id === dialogId);
+    users[user] = {
+      ...users[user],
+      userName: userName.value,
+      userEmail: userEmail.value,
+      userPhone: userPhone.value,
+    };
+  } else {
+    if (userName.value.length < 2) {
+      document.getElementById("error-name").innerText = "Tên phải ≥ 2 ký tự";
+      return;
+    }
+    if (userPhone.value.length < 10 || userPhone.value.length > 11) {
+      document.getElementById("error-phone").innerText =
+        "SĐT phải 10–11 chữ số";
+      return;
+    }
+    const user = {
+      id: Date.now(),
+      userName: userName.value,
+      userEmail: userEmail.value,
+      userPhone: userPhone.value,
+    };
+    users.push(user);
   }
-  if (userPhone.length < 10 || userPhone.length > 11) {
-    document.getElementById("error-phone").innerText = "SĐT phải 10–11 chữ số";
-    return;
-  }
-  const user = {
-    id: Date.now(),
-    userName,
-    userEmail,
-    userPhone,
-  };
-  users.push(user);
+
   saveUser();
   closeDialog();
   renderUser();
+});
+
+//xử lý sự kiện click outsite -> close modal dialog
+dialog.addEventListener("click", function (e) {
+  if (e.target === dialog) {
+    closeDialog();
+  }
+});
+
+dialogDelete.addEventListener("click", function (e) {
+  if (e.target === dialogDelete) {
+    closeDialog();
+  }
+});
+
+window.addEventListener("storage", (e) => {
+  if (e.key === "user") {
+    users = JSON.parse(localStorage.getItem("user"));
+    renderUser();
+  }
 });
